4518ff10a3c7daf19042d621f2514066d7bcec6f
Urosh <urosh_h@protonmail.com>
2023-06-28 20:45:43 +0200

    Add max load constants

--- src/LoadRequirement.ts
+++ src/LoadRequirement.ts
@@ -3,4 +3,7 @@ interface LoadRequirement {
     memoryLoad: number;
 };
 
-export { LoadRequirement }
+const MAX_CPU_LOAD: number = 100;
+const MAX_MEM_LOAD: number = 100;
+
+export { LoadRequirement, MAX_CPU_LOAD, MAX_MEM_LOAD }
