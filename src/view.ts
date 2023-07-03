f856cefebe7dac11547ff17679417650f3f699f6
Urosh <urosh_h@protonmail.com>
2023-07-03 09:55:14 +0200

    max load fields

--- src/view.ts
+++ src/view.ts
@@ -1,11 +1,17 @@
 function createView(): HTMLDivElement {
     let container = document.createElement("div");
+    let controlsContainer = document.createElement("div");
     let cpuLoadInput = document.createElement("input");
     let memLoadInput = document.createElement("input");
+    let maxCpuLoadInput = document.createElement("input");
+    let maxMemLoadInput = document.createElement("input");
     let numRequestsLabel = document.createElement("label");
     let numRequests = document.createElement("input");
     let servers = document.createElement("div");
     let startButton = document.createElement("button");
+
+    controlsContainer.style.display = "flex";
+    controlsContainer.style.flexDirection = "column";
 
     startButton.innerText = "Start simulation";
 
@@ -24,15 +30,26 @@ function createView(): HTMLDivElement {
     cpuLoadInput.type = "number";
     cpuLoadInput.placeholder = "Minimum CPU load for task";
 
+    maxCpuLoadInput.classList.add("cpu-load");
+    maxCpuLoadInput.type = "number";
+    maxCpuLoadInput.placeholder = "Maximum CPU load for task";
+
     memLoadInput.classList.add("mem-load");
     memLoadInput.type = "number";
     memLoadInput.placeholder = "Minimum MEM load for task";
 
-    container.appendChild(cpuLoadInput);
-    container.appendChild(memLoadInput);
-    container.appendChild(numRequestsLabel);
-    container.appendChild(numRequests);
-    container.appendChild(startButton);
+    maxMemLoadInput.classList.add("mem-load");
+    maxMemLoadInput.type = "number";
+    maxMemLoadInput.placeholder = "Maximum MEM load for task";
+
+    controlsContainer.appendChild(cpuLoadInput);
+    controlsContainer.appendChild(memLoadInput);
+    controlsContainer.appendChild(maxCpuLoadInput);
+    controlsContainer.appendChild(maxMemLoadInput);
+    controlsContainer.appendChild(numRequestsLabel);
+    controlsContainer.appendChild(numRequests);
+    controlsContainer.appendChild(startButton);
+    container.appendChild(controlsContainer);
     container.appendChild(servers);
 
     return container;
