964d933530ea714a7f50b076c7ca71fd1a8a2026
Urosh <urosh_h@protonmail.com>
2023-09-19 22:57:05 +0200

    minor changes

--- src/LoadBalancer.ts
+++ src/LoadBalancer.ts
@@ -1,94 +1,153 @@
-import { Observer, Subject, Subscription, asapScheduler, concatMap, filter, from, reduce } from "rxjs";
-import { Server } from "./Server";
-import { IncomingRequest } from "./IncomingRequest";
-import { MAX_CPU_LOAD, MAX_MEM_LOAD } from "./LoadRequirement";
+import {
+  BehaviorSubject,
+  EMPTY,
+  Observable,
+  Observer,
+  Subject,
+  Subscription,
+  asapScheduler,
+  catchError,
+  delay,
+  filter,
+  map,
+  merge,
+  switchMap,
+  throwError,
+  withLatestFrom,
+} from 'rxjs';
+import { Server } from './Server';
+import { IncomingRequest } from './IncomingRequest';
+import { LoadRequirement, MAX_CPU_LOAD, MAX_MEM_LOAD } from './LoadRequirement';
+
+type ServerState = LoadRequirement;
 
 class LoadBalancer implements Observer<IncomingRequest> {
-    private servers: Array<Server>;
-    private requestEmitter: Subject<IncomingRequest>;
-    private sub: Subscription;
-    private nextId: number = 1;
-    private parent: HTMLElement;
+  private servers: Array<Server> = [];
+  private requestEmitter: Subject<IncomingRequest> =
+    new Subject<IncomingRequest>();
+  private bestServer$: BehaviorSubject<Server> | null = null;
+  private serverStates$: Array<
+    Observable<{ index: number; state: ServerState }>
+  >;
+  private requestSubscription: Subscription = Subscription.EMPTY;
+  private serverStateSubscription: Subscription = Subscription.EMPTY;
+  private nextId: number = 1;
+  private parent: HTMLElement;
 
-    constructor(parent?: HTMLElement) {
-        if(parent) {
-            this.parent = parent;
-        }
-        else {
-            parent = document.body;
-        }
-        this.servers = new Array<Server>(
-            new Server(this.nextId++, this)
-        );
-        this.servers[this.servers.length - 1].draw(parent);
-        this.requestEmitter = new Subject();
-        this.sub = this.requestEmitter.subscribe(request => {
-            let server: Server; // server that's responsible for handling the request
-
-            from(this.servers).pipe(
-                filter((server) => {
-                    return server.load.cpuLoad + request.loadRequirements.cpuLoad < MAX_CPU_LOAD &&
-                        server.load.memoryLoad + request.loadRequirements.memoryLoad < MAX_MEM_LOAD;
-                }),
-                reduce((acc, current) => {
-                    return this.minServer(acc, current);
-                })
-            ).subscribe(srv => server = srv);
-
-            /*
-                If no servers are able to handle the request,
-                create a new instance and forward the request to it
-            */
-            if (!server) {
-                server = this.addNewServer();
-            }
-            asapScheduler.schedule(() => server.next(request));
-        });
+  constructor(parent?: HTMLElement) {
+    if (parent) {
+      this.parent = parent;
+    } else {
+      parent = document.body;
     }
+    this.addNewServer();
+    this.requestSubscription = this.requestEmitter
+      .pipe(
+        withLatestFrom(this.bestServer$),
+        filter(([request, server]) => server !== null || server !== undefined),
+        switchMap(([request, server]) => {
+          if (!server) return EMPTY;
+          if (
+            server.load.cpuLoad + request.loadRequirements.cpuLoad <
+              MAX_CPU_LOAD &&
+            server.load.memoryLoad + request.loadRequirements.memoryLoad <
+              MAX_MEM_LOAD
+          ) {
+            asapScheduler.schedule(() => server.next(request));
+          } else {
+            const server = this.addNewServer();
+            asapScheduler.schedule(() => server.next(request));
+          }
+          return EMPTY;
+        }),
+        catchError((error) => {
+          console.error(`'error: ${error}`);
+          return throwError(() => error);
+        })
+      )
+      .subscribe();
+  }
 
-    /*
+  /*
         Incoming Request, emit to a server
     */
-    next(value: IncomingRequest) {
-        this.requestEmitter.next(value);
-    }
-    error: (err: any) => void;
-    complete() {
-        this.sub.unsubscribe();
-    }
+  next(value: IncomingRequest) {
+    this.requestEmitter.next(value);
+  }
+  error: (err: any) => void;
+  complete() {
+    this.requestSubscription.unsubscribe();
+  }
 
-    /*
+  private minLoad(first: Server, second: Server): Server {
+    return first.load.cpuLoad < second.load.cpuLoad &&
+      first.load.memoryLoad < second.load.memoryLoad
+      ? first
+      : second;
+  }
+
+  /*
         Remove provided server instance from the DOM
         and from the server list
     */
-    remove(server: Server) {
-        let domElem = document.getElementById(`${server.id}`);
-        if (domElem) {
-            domElem.classList.add("fadeOut");
-            setTimeout(() => {domElem.remove(); }, 1000);
-        }
-        let serverToRemove = this.servers.find(srv => srv.id == server.id);
-        if (serverToRemove) {
-            let idx = this.servers.indexOf(serverToRemove);
-            this.servers.splice(idx, 1);
-        }
+  remove(server: Server) {
+    if (server.id == this.bestServer$.value.id) {
+      const nextBest = this.servers
+        .filter((x) => x.id != server.id)
+        .reduce((acc, curr) => this.minLoad(acc, curr));
+      this.bestServer$.next(nextBest);
     }
+    let domElem = document.getElementById(`${server.id}`);
+    if (domElem) {
+      domElem.classList.add('fadeOut');
+      setTimeout(() => {
+        domElem.remove();
+      }, 1000);
+    }
+    let serverToRemove = this.servers.find((srv) => srv.id == server.id);
+    if (serverToRemove) {
+      const idx = this.servers.indexOf(serverToRemove);
+      this.servers.splice(idx, 1);
+    }
+  }
 
-    /*
-        Return a server with minimal load
-    */
-    private minServer(first: Server, second: Server): Server {
-        return first.load.cpuLoad < second.load.cpuLoad &&
-            first.load.memoryLoad < second.load.memoryLoad ?
-            first : second;
-    }
+  private watchServers() {
+    this.serverStates$ = this.servers.map((server, index) =>
+      server.state$.asObservable().pipe(map((state) => ({ index, state })))
+    );
+    this.serverStateSubscription.unsubscribe();
+    this.serverStateSubscription = merge(...this.serverStates$)
+      .pipe(
+        withLatestFrom(this.bestServer$.asObservable()),
+        switchMap(([serverInfo, best]) => {
+          if (!best) return EMPTY;
+          if (
+            serverInfo.state.cpuLoad < best.load.cpuLoad &&
+            serverInfo.state.memoryLoad < best.load.memoryLoad
+          ) {
+            this.bestServer$.next(this.servers[serverInfo.index]);
+          }
+          return EMPTY;
+        }),
+        catchError((error) => {
+          console.error(`'error: ${error}`);
+          return throwError(() => error);
+        })
+      )
+      .subscribe();
+  }
 
-    private addNewServer(): Server {
-        let server = new Server(this.nextId++, this);
-        this.servers.push(server);
-        server.draw(this.parent);
-        return server;
+  private addNewServer(): Server {
+    const server = new Server(this.nextId++, this);
+    this.servers.push(server);
+    if (!this.bestServer$ || this.bestServer$.getValue() === undefined) {
+      this.bestServer$ = new BehaviorSubject(server);
+      this.bestServer$.next(server);
     }
+    this.watchServers();
+    server.draw(this.parent);
+    return server;
+  }
 }
 
-export { LoadBalancer }
+export { LoadBalancer };
--- src/Server.ts
+++ src/Server.ts
@@ -1,92 +1,99 @@
-import { Observer, Subject, asyncScheduler, throwError, timeout } from "rxjs";
-import { IncomingRequest } from "./IncomingRequest";
-import { LoadRequirement } from "./LoadRequirement";
-import { LoadBalancer } from "./LoadBalancer";
-import { ServerPreviewComponent } from "./components/ServerPreviewComponent";
-import { generateRandom, setBackground } from "./utils";
+import { Observer, Subject, asyncScheduler, throwError, timeout } from 'rxjs';
+import { IncomingRequest } from './IncomingRequest';
+import { LoadRequirement } from './LoadRequirement';
+import { LoadBalancer } from './LoadBalancer';
+import { ServerPreviewComponent } from './components/ServerPreviewComponent';
+import { generateRandom, setBackground } from './utils';
 
 type ServerLoad = LoadRequirement;
 
-
 class Server implements Observer<IncomingRequest> {
-    id: number;
-    load: ServerLoad;
-    private display: ServerPreviewComponent;
-    private loadBalancer: LoadBalancer; // necessary for reporting inactivity
-    private state$: Subject<ServerLoad>;
+  id: number;
+  load: ServerLoad;
+  private display: ServerPreviewComponent;
+  private loadBalancer: LoadBalancer; // necessary for reporting inactivity
+  public state$: Subject<ServerLoad>;
 
-    constructor(id: number, loadBalancer: LoadBalancer) {
-        this.id = id;
-        this.loadBalancer = loadBalancer;
-        this.display = new ServerPreviewComponent(this.id);
-        this.load = {
-            cpuLoad: 0,
-            memoryLoad: 0
-        };
-        this.state$ = new Subject<ServerLoad>();
-        this.startStateManagement();
-    }
+  constructor(id: number, loadBalancer: LoadBalancer) {
+    this.id = id;
+    this.loadBalancer = loadBalancer;
+    this.display = new ServerPreviewComponent(this.id);
+    this.load = {
+      cpuLoad: 0,
+      memoryLoad: 0,
+    };
+    this.state$ = new Subject<ServerLoad>();
+    this.startStateManagement();
+  }
 
-    draw(parent: HTMLElement) {
-        this.display.draw(parent);
-    }
+  draw(parent: HTMLElement) {
+    this.display.draw(parent);
+  }
 
-    next(value: IncomingRequest) {
-        this.handleRequest(value);
-    }
+  next(value: IncomingRequest) {
+    this.handleRequest(value);
+  }
 
-    error: (err: any) => void;
-    complete: () => void;
+  error: (err: any) => void;
+  complete: () => void;
 
-    /*
+  /*
         If server state hasn't been changed for 10sec
         (no incoming requests to handle)
         this server should not be active anymore
     */
-    private startStateManagement() {
-        this.state$.pipe(
-            timeout({
-                each: 10_000,
-                with: () => throwError(() => new Error(`Server ${this.id} inactive for 10 seconds`))
-            })
-        ).subscribe({
-            next: (requirements) => {
-                this.display.cpuBar.barContent.style.height = `${requirements.cpuLoad}%`;
-                setBackground(this.display.cpuBar.barContent, requirements.cpuLoad);
-                this.display.memBar.barContent.style.height = `${requirements.memoryLoad}%`;
-                setBackground(this.display.memBar.barContent, requirements.memoryLoad);
-            },
-            error: (_: Error) => {
-                this.loadBalancer.remove(this);
-            }
-        });
-    }
+  private startStateManagement() {
+    this.state$
+      .pipe(
+        timeout({
+          each: 10_000,
+          with: () =>
+            throwError(
+              () => new Error(`Server ${this.id} inactive for 10 seconds`)
+            ),
+        })
+      )
+      .subscribe({
+        next: (requirements) => {
+          this.display.cpuBar.barContent.style.height = `${requirements.cpuLoad}%`;
+          setBackground(this.display.cpuBar.barContent, requirements.cpuLoad);
+          this.display.memBar.barContent.style.height = `${requirements.memoryLoad}%`;
+          setBackground(
+            this.display.memBar.barContent,
+            requirements.memoryLoad
+          );
+        },
+        error: (_: Error) => {
+          this.loadBalancer.remove(this);
+        },
+      });
+  }
 
-    private handleLoad(requirements: LoadRequirement) {
-        this.load.cpuLoad += requirements.cpuLoad;
-        this.load.memoryLoad += requirements.memoryLoad;
-        this.state$.next(this.load);
-    }
+  private handleLoad(requirements: LoadRequirement) {
+    this.load.cpuLoad += requirements.cpuLoad;
+    this.load.memoryLoad += requirements.memoryLoad;
+    this.state$.next(this.load);
+  }
 
-    private releaseLoad(requirements: LoadRequirement) {
-        this.load.cpuLoad -= requirements.cpuLoad;
-        this.load.memoryLoad -= requirements.memoryLoad;
-        this.state$.next(this.load);
-    }
+  private releaseLoad(requirements: LoadRequirement) {
+    this.load.cpuLoad -= requirements.cpuLoad;
+    this.load.memoryLoad -= requirements.memoryLoad;
+    this.state$.next(this.load);
+  }
 
-    private handleRequest(request: IncomingRequest) {
-        let serializedRequest = JSON.stringify(request);
-        console.log(`Server ${this.id} handling request ${serializedRequest}`);
-        this.handleLoad(request.loadRequirements);
-        /*
+  private handleRequest(request: IncomingRequest) {
+    let serializedRequest = JSON.stringify(request);
+    //console.log(`Server ${this.id} handling request ${serializedRequest}`);
+    this.handleLoad(request.loadRequirements);
+    /*
             Simulate request handling as an asynchronous operation
             with random time between 1-5 sec
         */
-        asyncScheduler.schedule(() => {
-            console.log(`Server ${this.id} finished handling request ${serializedRequest}`);
-            this.releaseLoad(request.loadRequirements);
-        }, generateRandom(1000, 5000));
-    }
+    asyncScheduler.schedule(() => {
+      //console.log(`Server ${this.id} finished handling request ${serializedRequest}`);
+      this.releaseLoad(request.loadRequirements);
+    }, generateRandom(1000, 5000));
+  }
 }
 
-export { Server }
+export { Server };
